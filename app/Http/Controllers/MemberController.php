054ccb4a56e3f48aaf1049d8a13d57a718ae6294
tessamidha <tessamidha@gmail.com>
2025-01-08 11:41:29 +0700

    Update MemberController.php

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -1,155 +1,23 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Models;
 
-use App\Models\Member;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-use function Symfony\Component\VarDumper\Dumper\esc;
-
-class MemberController extends Controller
+class Member extends Authenticatable
 {
-    public function __construct()
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function order()
     {
-        $this->middleware('auth:api')->except(['index']);
+        return $this->hasMany(Order::class);
     }
 
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function cart()
     {
-        $members = Member::all();
-
-        return response()->json([
-            'data' => $members
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // dd($request->all());
-        $validator = Validator::make($request->all(),[
-            'nama_member' => 'required',
-            'provinsi' => 'required',
-            'kabupaten' => 'required',
-            'kecamatan' => 'required',
-            'detail_alamat' => 'required',
-            'no_hp' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|same:konfirmasi_password',
-            'konfirmasi_password' => 'required|same:password',
-            
-        ]);
-        if ($validator->fails()) {
-            return response()->json(
-                $validator->errors(),422
-            );
-        }
-
-        $input = $request->all();
-        $input['password'] = bcrypt($request->password);
-        $Member = Member::create($input);
-
-        return response()->json([
-            'data' => $Member
-        ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Member  $Member
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Member $Member)
-    {
-        return response()->json([
-            "data" => $Member
-        ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Member  $Member
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Member $Member)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Member  $Member
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Member $Member)
-    {
-        $validator = Validator::make($request->all(),[
-            'nama_member' => 'required',
-            'provinsi' => 'required',
-            'kabupaten' => 'required',
-            'kecamatan' => 'required',
-            'detail_alamat' => 'required',
-            'no_hp' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(
-                $validator->errors(),422
-            );
-        }
-
-        $input = $request->all();  
-
-        
-
-        $Member->update($input);
-
-        return response()->json([
-            'message' => 'success',
-            'data' => $Member
-        ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Member  $Member
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Member $Member)
-    {
-        $Member->delete();
-        
-        return response()->json([
-            'message' => 'success'
-        ]);
+        return $this->hasMany(Cart::class);
     }
 }
