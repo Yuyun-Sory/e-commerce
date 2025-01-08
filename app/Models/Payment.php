cf08b90381e4ac8b54cb7ab93b6c4a244c7d3a74
tessamidha <tessamidha@gmail.com>
2025-01-08 11:50:46 +0700

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,24 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
-
-    // Menambahkan atribut yang diizinkan untuk mass assignment
-    protected $fillable = [
-        'id_order',      // id_order perlu ditambahkan di sini
-        'id_member',
-        'jumlah',
-        'provinsi',
-        'kabupaten',
-        'kecamatan',
-        'detail_alamat',
-        'status',
-        'no_rekening',
-        'atas_nama',
-    ];
-
-    // Relasi dengan model Order
-    public function order()
+    public function member()
     {
         return $this->belongsTo(Order::class, 'id_order', 'id');
     }
+    
 }
